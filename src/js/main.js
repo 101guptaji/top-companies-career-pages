2b52d9f02b8a8f17c8d414d31ed3bc8fd17c1503
Himanshu Gupta <101guptaji@gmail.com>
2025-02-04 23:13:09 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,30 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+// Description: Open external links in a new tab and PDF links in a new tab
+
+// Open external links in a new window
+function external_new_window() {
+    for(let c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+        let b = c[a];
+        if(b.getAttribute("href") && b.host !== location.host) {
+            b.target = "_blank";
+            b.rel = "noopener";
+        }
+    }
+}
+
+function apply_rules() {
+    external_new_window();
+}
+
+if (typeof document$ !== "undefined") {
+    // Compatibility with mkdocs-material's instant loading feature
+    document$.subscribe(function() {
+        apply_rules();
+    });
+} else {
+    // For browsers without mkdocs-material's instant loading feature
+    document.addEventListener("DOMContentLoaded", function() {
+        apply_rules();
+    });
+}
